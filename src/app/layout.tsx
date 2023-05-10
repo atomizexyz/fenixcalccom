4bfb6b232d714a71c8b13ef5123fd76a790ddbf0
Joe Blau <im@joeblau.com>
2023-05-10 10:22:22 -0700

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Fenix•Calc",
   description: "Fenix Calc is a tool to help you calculate your Fenix payout.",
+  icons: {
+    icon: [{ url: "/images/icons/icon.png" }, new URL("/images/icons/icon.png", "https://fenixcalc.com")],
+    shortcut: ["/images/icons/favicon.ico"],
+    apple: [
+      { url: "/images/icons/icon@60.png" },
+      { url: "/images/icons/icon@152.png", sizes: "152x152", type: "image/png" },
+      { url: "/images/icons/icon@180.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "apple-touch-icon-precomposed",
+        url: "/images/icons/icon@180.png",
+      },
+    ],
+  },
+  category: "cryptocurrency",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
